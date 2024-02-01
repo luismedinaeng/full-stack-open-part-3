435a317dc2fc03e93a46df67f866104ae14400be
luismedinaeng <medinaluis.fel@gmail.com>
2024-01-31 23:02:21 -0500

    [Add]Part 3.1 to 3.6

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let phonebook = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${phonebook.length} people</p><p>${Date()}`)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(phonebook)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = phonebook.find(ph => ph.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    phonebook = phonebook.filter(ph => ph.id !== id)
+
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const id = Math.floor(Math.random() * 10000)
+    return id
+}
+
+const existPerson = (name) => {
+    const person = phonebook.find((p) => p.name === name)
+    return person ? true : false
+} 
+
+app.post('/api/persons', (request, response) => {
+    const body  = request.body
+
+    if(!body.number || !body.name) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    if (existPerson(body.name)) {
+        return response.status(400).json({
+            error: 'name already exists'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number, 
+        id: generateId()
+    }
+    
+    phonebook = phonebook.concat(person)
+
+    response.json(person)
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
